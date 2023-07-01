12c255c658f946f7310a885f45c7c254b352c650
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-07-01 14:59:05 +0300

    create places endpoint

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -23,6 +23,7 @@ from . import views
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', views.show_main)
+                  path('', views.show_main),
+                  path('places/<int:place_id>/', views.place_view),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                                            document_root=settings.MEDIA_ROOT)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 
 from places.models import Place
 
@@ -6,7 +7,7 @@ from places.models import Place
 def show_main(request):
     locations = Place.objects.all()
     locations_format_geojson = []
-    for num, location in enumerate(locations):
+    for location in locations:
         formatted_location = {
             "type": "Feature",
             "geometry": {
@@ -15,10 +16,31 @@ def show_main(request):
             },
             "properties": {
                 "title": location.title,
-                "placeId": num,
+                "placeId": location.id,
                 "detailsUrl": ""
             }
         }
         locations_format_geojson.append(formatted_location)
-    context = {'geojson': {"type": "FeatureCollection", 'features': locations_format_geojson}}
+
+    context = {'geojson':
+        {
+            "type": "FeatureCollection",
+            'features': locations_format_geojson
+        }
+    }
     return render(request, 'index.html', context=context)
+
+
+def place_view(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    context = {
+        'title': place.title,
+        'img': [image.image.url for image in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lat': place.lat,
+            'lng': place.lng,
+        }
+    }
+    return JsonResponse(context, safe=False, json_dumps_params={'indent': 2, 'ensure_ascii': False})
